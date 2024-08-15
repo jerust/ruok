98f3aa8ff91356e66dd1fc38e87fe32609d3a0df
jerust <jerust@163.com>
2024-08-15 15:49:25 +0800

    feat(enum): 新增枚举类型案例,

--- types/src/enum_type.rs
+++ types/src/enum_type.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use std::collections::{HashMap, HashSet};
+use std::f64::consts;
 use std::fmt;
 use std::hash::Hash;
 
@@ -8,6 +9,29 @@ enum HttpStatusCode {
     SwitchProtocol, // 如果不显式分配第一个变体的值, 那么它的值等于0
     NotFound = 404,
     GatewayTimeout, // 如果不显式分配非第一个变体的值, 那么它的值等于上一个变体的值加1
+}
+
+enum SchedulerState<Job, Pid>
+where
+    Job: Eq + Hash,
+    Pid: Eq + Hash,
+{
+    Pending(HashSet<Job>),
+    Running(HashMap<Pid, Vec<Job>>),
+}
+
+enum Shape {
+    Circle { radius: f64 },
+    Rectangle { width: f64, height: f64 },
+}
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Circle { radius } => consts::PI * radius * radius,
+            Shape::Rectangle { width, height } => width * height,
+        }
+    }
 }
 
 #[repr(u8)] // 指定枚举变体的底层类型为u8
@@ -25,15 +49,6 @@ impl<'a> fmt::Display for ProgramLanguage<'a> {
             Self::Rest(lang, rank) => write!(f, "Top{}: {}", rank, lang),
         }
     }
-}
-
-enum SchedulerState<Job, Pid>
-where
-    Job: Eq + Hash,
-    Pid: Eq + Hash,
-{
-    Pending(HashSet<Job>),
-    Running(HashMap<Pid, Vec<Job>>),
 }
 
 #[cfg(test)]
